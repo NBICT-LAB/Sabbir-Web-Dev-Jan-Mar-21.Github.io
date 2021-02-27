577f823aed9682e28362445395ddc96ff64c4da8
Abu Shaid Sabbir <75726900+sabbir102629@users.noreply.github.com>
2021-02-27 06:46:24 -0800

    feb 27.0

---
+++ extra/practice.js
@@ -0,0 +1,141 @@
+/*let js = 'awesome';
+console.log(js);
+console.log(17-31+20*2);
+let firstName ='Abu shaid ';
+let lastName = 'Sabbir';
+console.log(firstName + ' ' + lastName + 'is a student of computer techlnology.'
++ ' '+ lastName + ' is my nickname');
+ let x = 'abushaid';
+ console.log(typeof(x));
+ let country = 'Bangladesh';
+ let continent = 'asia';
+ let population = 'twenty core';
+ console.log(country + ' ' + continent +' ' + population);
+ let isIsland = true;
+ let language;
+
+ console.log(typeof(isIsland));
+ console.log(typeof(population));
+ console.log(typeof(country));
+ console.log(typeof(language));
+ */
+/*
+let age = 19;
+console.log(age);
+
+
+const birthYear = 2002;
+// birthYear = 2001;
+console.log(birthYear);
+
+let myName;
+// const yourName;
+var mySubject = 'CSE';
+console.log(mySubject);
+myName = 'Abu Shaid';
+console.log(myName);
+// mathmatical operator 
+
+const now = 2021;
+const abushaidAge = now - 2002;
+ const royAge = now - 2005;
+
+ console.log(abushaidAge , royAge);
+ console.log(abushaidAge * 2 , abushaidAge /10 , 2 ** 4);
+
+ const firstName = 'NBICT';
+ const lastName = 'LAB';
+ console.log(firstName + ' ' + lastName);
+  
+
+ // assignment operator
+
+ let x = 7+5;
+ x += 2;
+ x -= 3;
+ x++;
+ x--;
+ console.log(x);
+
+ //comparison operator 
+
+ console.log(abushaidAge > royAge);
+ console.log(abushaidAge >= 20);
+
+ const country = 'Bangladesh';
+ const continent = 'Asia';
+ let population = 180;
+ console.log(country , continent , population);
+  
+ const isIsland = 'true';
+ const language = 'Bangla';
+
+ console.log(isIsland , language);
+ console.log(typeof(country));
+ console.log(typeof(isIsland));
+ console.log(typeof(population));
+ console.log(typeof(language));
+
+ const firstHalf = population / 2;
+ const secondHalf = population / 2;
+  
+ console.log(firstHalf , secondHalf);
+ console.log(++population);
+
+ let finpopulation = 6;
+ console.log(population > finpopulation);
+
+ let averagepopulation = 33;
+ console.log(averagepopulation > population);
+
+ let description = country + ' ' + 
+ 'is in ' + ' ' + continent + '  ' +' and it is ' + population + ' ' + 'people speak' + ' ' + language + '.';
+ console.log(description);
+ */
+/*
+ const now = 2037;
+ const ageJonus = now - 1992;
+ const ageSara = now - 1998;
+
+ const average = (ageJonus + ageSara) / 2;
+
+ console.log(ageJonus , ageSara , average);
+
+console.log(now - 1992 > now - 1998);
+
+const x = y = 25 - 10 + 5;
+console.log(x);
+
+const massMark = 78;
+const massJohn = 92;
+const heightMark = 1.69;
+const heightJohn = 1.95;
+
+console.log(massMark , massJohn , heightJohn , heightMark);
+
+const Markbmi = massMark / (heightMark * heightMark);
+const Johnbmi = massJohn / (heightJohn * heightJohn);
+
+let markHigherBMI = Markbmi > Johnbmi;
+console.log(Markbmi , Johnbmi , markHigherBMI);
+
+*/
+
+
+const massMark = 95;
+const massJohn = 85;
+const heightMark = 1.88;
+const heightJohn = 1.76;
+
+
+console.log(massMark , massJohn , heightMark , heightJohn);
+
+const MarkBMI = massMark / ( heightMark * heightMark);
+const JohnBMI = massJohn / (heightJohn * heightJohn);
+
+
+let markHigherBMI = MarkBMI > JohnBMI;
+
+
+console.log(MarkBMI , JohnBMI , markHigherBMI);
+
